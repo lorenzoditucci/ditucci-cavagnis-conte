ceb09e1664125c05ad8269f6091fdff58ec1278a
leonardo.cavagnis@gmail.com <leonardo.cavagnis@gmail.com@2a022a1e-1ce0-e600-22e9-f5664e8181da>
2014-01-05 09:42:59 +0000

    creazione progetto Web

---
+++ trunk/se2-traveldream-ditucci-cavagnis-conteWeb/src/it/polimi/traveldream/web/beans/LogoutBean.java
@@ -0,0 +1,17 @@
+package it.polimi.traveldream.web.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@RequestScoped
+public class LogoutBean {
+	
+	public String logout() {
+	    FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+	    return "/home?faces-redirect=true";
+	  }
+
+
+}
