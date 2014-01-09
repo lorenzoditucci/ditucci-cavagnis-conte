96aba806054f76651ec3b54f7942613e7ab1b7a6
lorenzo.ditucci@gmail.com <lorenzo.ditucci@gmail.com@2a022a1e-1ce0-e600-22e9-f5664e8181da>
2014-01-09 19:29:20 +0000

    aggiunta bozza di ricerca in gift list

--- se2-traveldream-ditucci-cavagnis-conteEJBClient/ejbModule/it/polimi/traveldream/ejb/management/dto/GiftListDTO.java
+++ se2-traveldream-ditucci-cavagnis-conteEJBClient/ejbModule/it/polimi/traveldream/ejb/management/dto/GiftListDTO.java
@@ -1,5 +1,16 @@
 package it.polimi.traveldream.ejb.management.dto;
 
-public class GiftListDTO {
+import org.hibernate.validator.constraints.NotEmpty;
 
+public class GiftListDTO {
+	@NotEmpty
+	private int idGiftList;
+
+	public int getIdGiftList() {
+		return idGiftList;
+	}
+
+	public void setIdGiftList(int idGiftList) {
+		this.idGiftList = idGiftList;
+	}
 }
