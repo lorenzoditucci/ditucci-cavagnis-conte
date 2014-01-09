dd919639dcf01ed2f55acf5d6eb61615f06ccf0e
leonardo.cavagnis@gmail.com <leonardo.cavagnis@gmail.com@2a022a1e-1ce0-e600-22e9-f5664e8181da>
2014-01-09 23:01:00 +0000

    Aggiunta procedura di aggiunta di un prodotto base di tipo Volo da parte del Dipendente

---
+++ se2-traveldream-ditucci-cavagnis-conteEJBClient/ejbModule/it/polimi/traveldream/ejb/management/VoloMgr.java
@@ -0,0 +1,13 @@
+package it.polimi.traveldream.ejb.management;
+
+import javax.ejb.Local;
+
+import it.polimi.traveldream.ejb.management.dto.VoloDTO;
+
+
+@Local
+public interface VoloMgr {
+	
+	public void save(VoloDTO volo);
+
+}
--- se2-traveldream-ditucci-cavagnis-conteEJBClient/ejbModule/it/polimi/traveldream/ejb/management/dto/VoloDTO.java
+++ se2-traveldream-ditucci-cavagnis-conteEJBClient/ejbModule/it/polimi/traveldream/ejb/management/dto/VoloDTO.java
@@ -1,5 +1,85 @@
 package it.polimi.traveldream.ejb.management.dto;
 
+
+
+import java.util.Date;
+
+import javax.validation.constraints.Future;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
 public class VoloDTO {
+
+
+	@NotEmpty
+	private String compagnia;
+
+	@NotEmpty
+    private String cittaPartenza;
+	
+	@NotEmpty
+    private String cittaArrivo;
+	
+	@NotNull
+	@Future
+    private Date dataPartenza;
+	
+	@NotNull
+	@Future
+    private Date dataArrivo;
+	
+	@NotNull
+	@Min(0)
+    private Double costo;
+    
+	public String getCompagnia() {
+		return compagnia;
+	}
+
+	public void setCompagnia(String compagnia) {
+		this.compagnia = compagnia;
+	}
+
+	public String getCittaPartenza() {
+		return cittaPartenza;
+	}
+
+	public void setCittaPartenza(String cittaPartenza) {
+		this.cittaPartenza = cittaPartenza;
+	}
+
+	public String getCittaArrivo() {
+		return cittaArrivo;
+	}
+
+	public void setCittaArrivo(String cittaArrivo) {
+		this.cittaArrivo = cittaArrivo;
+	}
+	public Date getDataPartenza() {
+		return dataPartenza;
+	}
+
+	public void setDataPartenza(Date dataPartenza) {
+		this.dataPartenza = dataPartenza;
+	}
+
+	public Date getDataArrivo() {
+		return dataArrivo;
+	}
+
+	public void setDataArrivo(Date dataArrivo) {
+		this.dataArrivo = dataArrivo;
+	}
+
+
+	public Double getCosto() {
+		return costo;
+	}
+
+	public void setCosto(Double costo) {
+		this.costo = costo;
+	}
 
 }
