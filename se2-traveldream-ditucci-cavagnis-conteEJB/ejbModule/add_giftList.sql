5579f00d4f763a47580cc4c6c8b4824c8603a56e
lorenzo.ditucci@gmail.com <lorenzo.ditucci@gmail.com@2a022a1e-1ce0-e600-22e9-f5664e8181da>
2014-01-10 09:54:11 +0000

    prova su GiftList con ricerca su database funzionante.
    creazione di file sql per inserire dati in tabella gift list

---
+++ se2-traveldream-ditucci-cavagnis-conteEJB/ejbModule/add_giftList.sql
@@ -0,0 +1,3 @@
+insert into GiftList values ("1","lorenzo@lorenzo.it","Lorenzo");
+insert into GiftList values ("2","franco@franco.it","Franco");
+insert into GiftList values ("3","bargis@scafargis.it","scarafaggis");
