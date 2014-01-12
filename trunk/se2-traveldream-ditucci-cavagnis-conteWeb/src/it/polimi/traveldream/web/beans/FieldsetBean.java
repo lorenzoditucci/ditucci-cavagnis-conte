d6b3071e0f8987d56b52cb65707962c155bcf809
lorenzo.ditucci@gmail.com <lorenzo.ditucci@gmail.com@2a022a1e-1ce0-e600-22e9-f5664e8181da>
2014-01-12 23:22:48 +0000

    vari miglioramenti grafici

--- trunk/se2-traveldream-ditucci-cavagnis-conteWeb/src/it/polimi/traveldream/web/beans/FieldsetBean.java
+++ trunk/se2-traveldream-ditucci-cavagnis-conteWeb/src/it/polimi/traveldream/web/beans/FieldsetBean.java
@@ -2,13 +2,28 @@ package it.polimi.traveldream.web.beans;
 
 
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
-import org.primefaces.event.ToggleEvent;
+import javax.json.stream.JsonParser.Event;
 
+import org.primefaces.event.ToggleEvent;
+import org.primefaces.model.Visibility;
+
+@ManagedBean(name="fieldsetBean")
+@RequestScoped
 public class FieldsetBean {
 
     public void handleToggle(ToggleEvent event) {
-        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Fieldset Toggled", "Visibility:" + event.getVisibility());
+    	String messaggio;
+    	event.getVisibility();
+		if(event.getVisibility().equals(Visibility.VISIBLE)){
+    		messaggio = new String("Visibile");
+    	}else {
+			messaggio = new String("Nascosto");
+		}
+    	
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Avviso", "Pacchetto " + messaggio);
 
         FacesContext.getCurrentInstance().addMessage(null, msg);
     }
