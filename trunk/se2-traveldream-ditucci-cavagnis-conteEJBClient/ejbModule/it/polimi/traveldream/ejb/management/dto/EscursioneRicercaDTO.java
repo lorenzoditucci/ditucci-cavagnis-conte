4013e01495144a182895d34c491f8684e82f6462
leonardo.cavagnis@gmail.com <leonardo.cavagnis@gmail.com@2a022a1e-1ce0-e600-22e9-f5664e8181da>
2014-01-12 12:58:15 +0000

    preparazione per ricerca escursione

---
+++ trunk/se2-traveldream-ditucci-cavagnis-conteEJBClient/ejbModule/it/polimi/traveldream/ejb/management/dto/EscursioneRicercaDTO.java
@@ -0,0 +1,80 @@
+package it.polimi.traveldream.ejb.management.dto;
+
+import java.util.Date;
+
+import javax.validation.constraints.Future;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+public class EscursioneRicercaDTO {
+
+	private int acquistato;
+
+	private String citta;
+
+	private double costo;
+	
+	private Date dataFine;
+
+	private Date dataInizio;
+
+	private String descrizione;
+
+	private String nome;
+
+	public int getAcquistato() {
+		return acquistato;
+	}
+
+	public void setAcquistato(int acquistato) {
+		this.acquistato = acquistato;
+	}
+
+	public String getCitta() {
+		return citta;
+	}
+
+	public void setCitta(String citta) {
+		this.citta = citta;
+	}
+
+	public double getCosto() {
+		return costo;
+	}
+
+	public void setCosto(double costo) {
+		this.costo = costo;
+	}
+
+	public Date getDataFine() {
+		return dataFine;
+	}
+
+	public void setDataFine(Date dataFine) {
+		this.dataFine = dataFine;
+	}
+
+	public Date getDataInizio() {
+		return dataInizio;
+	}
+
+	public void setDataInizio(Date dataInizio) {
+		this.dataInizio = dataInizio;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
