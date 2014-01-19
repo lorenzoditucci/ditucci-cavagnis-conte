e9f839eda43d58f1c93b828c6fedb13e366c8884
lorenzo.ditucci@gmail.com <lorenzo.ditucci@gmail.com@2a022a1e-1ce0-e600-22e9-f5664e8181da>
2014-01-19 16:54:25 +0000

    gestione dell'aggiornamento dell'acquisto di un prodotto base nella giftlist da parte di un utente non registrato

--- se2-traveldream-ditucci-cavagnis-conteWeb/src/it/polimi/traveldream/web/beans/ProdottoBaseBean.java
+++ se2-traveldream-ditucci-cavagnis-conteWeb/src/it/polimi/traveldream/web/beans/ProdottoBaseBean.java
@@ -1,9 +1,12 @@
 package it.polimi.traveldream.web.beans;
 
+import it.polimi.traveldream.ejb.management.ProdottoBaseMgr;
 import it.polimi.traveldream.ejb.management.dto.EscursioneDTO;
+import it.polimi.traveldream.ejb.management.dto.GiftListDTO;
 import it.polimi.traveldream.ejb.management.dto.PernottamentoDTO;
 import it.polimi.traveldream.ejb.management.dto.VoloDTO;
 
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -15,6 +18,8 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class ProdottoBaseBean {
 
+	@EJB
+	private ProdottoBaseMgr mgr;
     /**
      * Default constructor. 
      */
@@ -22,16 +27,27 @@ public class ProdottoBaseBean {
         // TODO Auto-generated constructor stub
     }
     
-    public String acquistaProdottoBase(VoloDTO volo){
+    public String acquistaProdottoBase(VoloDTO volo,GiftListDTO giftList){
+    	/**
+    	 * non so se andra' messo qua o ci mettiamo una pagina nel mezzo
+    	 * nel caso spostiamo il tutto in un altra funzione
+    	 */
+    	
+    	/**
+    	 * devo registrare che il prodotto base e' stato acquistato
+    	 */
+    	mgr.registraAcquisto(volo,giftList);
     	return "acquista?faces-redirect=true";
     }
     
     
-    public String acquistaProdottoBase(EscursioneDTO escursione){
+    public String acquistaProdottoBase(EscursioneDTO escursione,GiftListDTO giftList){
+    	mgr.registraAcquisto(escursione,giftList);
     	return "acquista?faces-redirect=true";
     }
     
-    public String acquistaProdottoBase(PernottamentoDTO pernottamento){
+    public String acquistaProdottoBase(PernottamentoDTO pernottamento,GiftListDTO giftList){
+    	mgr.registraAcquisto(pernottamento, giftList);
     	return "acquista?faces-redirect=true";
     }
 
