f6343c6b854aa6e0ddd65c8d43bcbe2951712eb8
gioconte91@gmail.com <gioconte91@gmail.com@2a022a1e-1ce0-e600-22e9-f5664e8181da>
2014-01-21 17:48:32 +0000

    Aggiornata visualizzazione Pacchetti in una gift list.

--- trunk/se2-traveldream-ditucci-cavagnis-conteEJB/ejbModule/it/polimi/traveldream/ejb/management/mgrbean/VisualizzaDettagliGLMgrBean.java
+++ trunk/se2-traveldream-ditucci-cavagnis-conteEJB/ejbModule/it/polimi/traveldream/ejb/management/mgrbean/VisualizzaDettagliGLMgrBean.java
@@ -1,13 +1,22 @@
 package it.polimi.traveldream.ejb.management.mgrbean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.ejb.EJBContext;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateful;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import it.polimi.traveldream.ejb.management.VisualizzaDettagliGLMgr;
+import it.polimi.traveldream.ejb.management.dto.GiftListDTO;
+import it.polimi.traveldream.ejb.management.dto.PacchettoDTO;
+import it.polimi.traveldream.ejb.management.dto.PernottamentoDTO;
+import it.polimi.traveldream.ejb.management.entity.GiftList;
+import it.polimi.traveldream.ejb.management.entity.Pernottamento;
 
 @Stateful
 @LocalBean
@@ -22,4 +31,22 @@ public class VisualizzaDettagliGLMgrBean implements VisualizzaDettagliGLMgr {
 	public VisualizzaDettagliGLMgrBean() {
 	// TODO Auto-generated constructor stub
 }
+	
+	/*
+	 *  public ArrayList<GiftListDTO> cercaGLperMail(String userEmail) {
+    	
+    	TypedQuery<GiftList> queryRicerca = em.createNamedQuery("cercaGiftListPerMail", GiftList.class);
+    	List<GiftList> listaGiftList =  queryRicerca.setParameter("mail", userEmail).getResultList();
+		
+    	return copiaListaQuery(listaGiftList);     
+    }
+    
+	 */
+	@Override 
+	public List<PernottamentoDTO> cercaPernottamentiDaPacchetto(PacchettoDTO pacchetto){
+		TypedQuery<Pernottamento> queryRicerca = em.createNamedQuery("Pernottamento.cercaPernottamentoIdPacchetto", Pernottamento.class);
+		List<Pernottamento> listaPernottamenti = queryRicerca.setParameter("idPacchetto", pacchetto.getIdPacchetto()).getResultList();
+		
+		return Pernottamento.copiaToPernottamentoDTO(listaPernottamenti);
+	}
 }
--- trunk/se2-traveldream-ditucci-cavagnis-conteEJBClient/ejbModule/it/polimi/traveldream/ejb/management/VisualizzaDettagliGLMgr.java
+++ trunk/se2-traveldream-ditucci-cavagnis-conteEJBClient/ejbModule/it/polimi/traveldream/ejb/management/VisualizzaDettagliGLMgr.java
@@ -1,8 +1,15 @@
 package it.polimi.traveldream.ejb.management;
 
+import it.polimi.traveldream.ejb.management.dto.PacchettoDTO;
+import it.polimi.traveldream.ejb.management.dto.PernottamentoDTO;
+
+import java.util.List;
+
 import javax.ejb.Local;
 
 @Local
 public interface VisualizzaDettagliGLMgr {
+
+	List<PernottamentoDTO> cercaPernottamentiDaPacchetto(PacchettoDTO pacchetto);
 	
 }
