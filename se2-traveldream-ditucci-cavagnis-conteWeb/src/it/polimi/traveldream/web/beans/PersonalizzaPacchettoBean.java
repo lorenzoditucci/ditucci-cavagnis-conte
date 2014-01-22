2a98529cbc7d47cc06d3ec41e48a9757bb6b0719
gioconte91@gmail.com <gioconte91@gmail.com@2a022a1e-1ce0-e600-22e9-f5664e8181da>
2014-01-22 08:10:40 +0000

    Work in progress. Personalizzazione pacchetto.

--- se2-traveldream-ditucci-cavagnis-conteWeb/src/it/polimi/traveldream/web/beans/PersonalizzaPacchettoBean.java
+++ se2-traveldream-ditucci-cavagnis-conteWeb/src/it/polimi/traveldream/web/beans/PersonalizzaPacchettoBean.java
@@ -1,7 +1,14 @@
 package it.polimi.traveldream.web.beans;
 
-import it.polimi.traveldream.ejb.management.dto.PacchettoDTO;
+import java.util.ArrayList;
+import java.util.List;
 
+import it.polimi.traveldream.ejb.management.GiftListMgr;
+import it.polimi.traveldream.ejb.management.VisualizzaDettagliGLMgr;
+import it.polimi.traveldream.ejb.management.dto.PacchettoDTO;
+import it.polimi.traveldream.ejb.management.dto.PernottamentoDTO;
+
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -9,6 +16,9 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean(name="personalizzaPacchettoBean")
 @SessionScoped
 public class PersonalizzaPacchettoBean {
+	
+	@EJB
+	private VisualizzaDettagliGLMgr glMgr;
 
 	private PacchettoDTO pacchetto;
 
@@ -23,6 +33,12 @@ public class PersonalizzaPacchettoBean {
 	public String personalizza(PacchettoDTO p){
 		this.pacchetto = p;
 		return "personalizza";
+	}
+	
+	public List<PernottamentoDTO> getPernottamenti(){
+		List<PernottamentoDTO> pernottamenti = new ArrayList<PernottamentoDTO>();
+		pernottamenti = glMgr.cercaPernottamentiDaPacchetto(pacchetto);
+		return pernottamenti;
 	}
 	
 	
