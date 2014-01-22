0bd274f12b0c5374a26fa54daf91b3cbe840a270
leonardo.cavagnis@gmail.com <leonardo.cavagnis@gmail.com@2a022a1e-1ce0-e600-22e9-f5664e8181da>
2014-01-22 21:02:28 +0000

    [work in progress] Modifica pacchetto

--- trunk/se2-traveldream-ditucci-cavagnis-conteEJB/ejbModule/it/polimi/traveldream/ejb/management/mgrbean/GestionePacchettoMgrBean.java
+++ trunk/se2-traveldream-ditucci-cavagnis-conteEJB/ejbModule/it/polimi/traveldream/ejb/management/mgrbean/GestionePacchettoMgrBean.java
@@ -18,10 +18,13 @@ import it.polimi.traveldream.ejb.management.dto.EscursioneDTO;
 import it.polimi.traveldream.ejb.management.dto.HotelDTO;
 import it.polimi.traveldream.ejb.management.dto.PacchettoDTO;
 import it.polimi.traveldream.ejb.management.dto.VoloDTO;
+import it.polimi.traveldream.ejb.management.entity.Citta;
+import it.polimi.traveldream.ejb.management.entity.Escursione;
+import it.polimi.traveldream.ejb.management.entity.GiftList;
 import it.polimi.traveldream.ejb.management.entity.Hotel;
 import it.polimi.traveldream.ejb.management.entity.Pacchetto;
-import it.polimi.traveldream.ejb.management.entity.User;
-import it.polimi.traveldream.ejb.management.entity.Pernottamento;;
+import it.polimi.traveldream.ejb.management.entity.Pernottamento;
+import it.polimi.traveldream.ejb.management.entity.Volo;
 
 @Stateless
 public class GestionePacchettoMgrBean implements GestionePacchettoMgr {
@@ -93,6 +96,7 @@ public class GestionePacchettoMgrBean implements GestionePacchettoMgr {
 		
 		try{
 		creaPacchettoMgr.controlloPacchetto(pacchetto);
+		aggiornaPersistenzaPacchetto(pacchetto);
 		return true;
 		}
 		catch(CoerenzaException e){
@@ -100,6 +104,78 @@ public class GestionePacchettoMgrBean implements GestionePacchettoMgr {
 		return false;
 		}
 		
+	}
+
+	private void aggiornaPersistenzaPacchetto(PacchettoDTO pacchettoAggiornato) {
+		/*Prelevo l'istanza corrente nel database*/
+		Pacchetto newPacchetto = em.find(Pacchetto.class, pacchettoAggiornato.getIdPacchetto());
+		/*aggiorno i parametri*/
+		newPacchetto.setNome(pacchettoAggiornato.getNome());
+		newPacchetto.setDescrizione(pacchettoAggiornato.getDescrizione());
+		newPacchetto.setDisponibilitaAttuale(pacchettoAggiornato.getDisponibilitaAttuale());
+		newPacchetto.setDisponibilitaMax(pacchettoAggiornato.getDisponibilitaMax());
+		newPacchetto.setDataInizio(pacchettoAggiornato.getDataInizio());
+		newPacchetto.setDataFine(pacchettoAggiornato.getDataFine());
+		newPacchetto.setCosto(pacchettoAggiornato.getCosto());
+		newPacchetto.setMail(pacchettoAggiornato.getMail());
+		/*aggiorno le relazioni*/
+		newPacchetto.setCittaDestinazione(new ArrayList<Citta>());
+		newPacchetto.setVoli(new ArrayList<Volo>());
+		newPacchetto.setEscursioni(new ArrayList<Escursione>());
+		newPacchetto.setGiftLists(new ArrayList<GiftList>());
+		
+		//association
+		List<Volo> voli = new ArrayList<Volo>();
+		for(int i=0; i<pacchettoAggiornato.getVoli().size(); i++){
+			 TypedQuery<Volo> queryRicerca = em.createNamedQuery("Volo.cercaVoloId", Volo.class);
+		 	 List<Volo> listaVolo = queryRicerca.setParameter("idVolo", pacchettoAggiornato.getVoli().get(i).getIdVolo()).getResultList();
+		 	 voli.add(listaVolo.get(0));	
+		}
+		newPacchetto.getVoli().addAll(voli);
+		
+		
+		List<Escursione> escursioni = new ArrayList<Escursione>();
+		for(int i=0; i<pacchettoAggiornato.getEscursioni().size(); i++){
+			 TypedQuery<Escursione> queryRicerca = em.createNamedQuery("Escursione.cercaEscursioneId", Escursione.class);
+		 	 List<Escursione> listaEscursione = queryRicerca.setParameter("idEscursione", pacchettoAggiornato.getEscursioni().get(i).getIdEscursione()).getResultList();
+		 	 escursioni.add(listaEscursione.get(0));	
+		}
+		
+		newPacchetto.getEscursioni().addAll(escursioni);
+		
+		
+		List<Citta> citta = new ArrayList<Citta>();
+		for(int i=0; i<pacchettoAggiornato.getCittaDestinazione().size(); i++){
+			 TypedQuery<Citta> queryRicerca = em.createNamedQuery("Citta.cercaCittaNome", Citta.class);
+		 	 List<Citta> listaCitta = queryRicerca.setParameter("nomeCitta", pacchettoAggiornato.getCittaDestinazione().get(i).getNome()).getResultList();
+		     citta.add(listaCitta.get(0));	
+		}
+		newPacchetto.getCittaDestinazione().addAll(citta);
+		/*aggiorna la tupla e i riferimenti con i nuovi valori*/
+		em.refresh(newPacchetto);
+	
+		/*eliminazione dei vecchi pernottamenti*/
+		TypedQuery<Pernottamento> queryRicercaPernottamenti = em.createNamedQuery("Pernottamento.cercaPernottamentoIdPacchetto", Pernottamento.class);
+		List<Pernottamento> listaPernottamento = queryRicercaPernottamenti.setParameter("idPacchetto", newPacchetto.getIdPacchetto()).getResultList();
+	
+	/*	for(int i=0; i<listaPernottamento.size(); i++){
+			em.remove(listaPernottamento.get(i));
+		}*/
+
+		
+		for(int i=0; i<pacchettoAggiornato.getPernotti().size(); i++){
+	
+			listaPernottamento.get(i).setDataInizio(pacchettoAggiornato.getPernotti().get(i).getDataInizio());
+			
+			listaPernottamento.get(i).setDataFine(pacchettoAggiornato.getPernotti().get(i).getDataFine());
+			TypedQuery<Hotel> queryRicerca = em.createNamedQuery("Hotel.cercaHotelId", Hotel.class);
+		 	List<Hotel> listaHotel = queryRicerca.setParameter("idHotel", pacchettoAggiornato.getPernotti().get(i).getHotel().getIdHotel()).getResultList();
+			
+		 	listaPernottamento.get(i).setHotel(listaHotel.get(0));
+		 	listaPernottamento.get(i).setPacchetto(newPacchetto);
+		 	
+		 	em.refresh(listaPernottamento.get(i));
+		}
 	}
 
 }
