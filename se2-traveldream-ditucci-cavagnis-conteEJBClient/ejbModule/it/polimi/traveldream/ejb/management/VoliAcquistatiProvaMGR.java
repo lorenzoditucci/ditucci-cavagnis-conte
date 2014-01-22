2ed1109ab520e6349ca100bcc36fa53c7b23cbf6
lorenzo.ditucci@gmail.com <lorenzo.ditucci@gmail.com@2a022a1e-1ce0-e600-22e9-f5664e8181da>
2014-01-22 18:43:07 +0000

    creazione funzioni x divisione voli venduti e non venduti

---
+++ se2-traveldream-ditucci-cavagnis-conteEJBClient/ejbModule/it/polimi/traveldream/ejb/management/VoliAcquistatiProvaMGR.java
@@ -0,0 +1,12 @@
+package it.polimi.traveldream.ejb.management;
+
+import it.polimi.traveldream.ejb.management.dto.VoliAcquistatiProvaDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface VoliAcquistatiProvaMGR {
+
+	List<VoliAcquistatiProvaDTO> cercaVoliAcquistati(int i);
+
+}
