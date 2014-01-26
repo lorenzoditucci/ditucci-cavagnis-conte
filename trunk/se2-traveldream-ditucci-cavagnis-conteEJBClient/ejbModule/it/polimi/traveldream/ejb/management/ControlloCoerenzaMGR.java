e944fdddd8cc0e110b019ecdb2e72e478b09df3b
gioconte91@gmail.com <gioconte91@gmail.com@2a022a1e-1ce0-e600-22e9-f5664e8181da>
2014-01-26 21:10:09 +0000

    Terminata personalizzazione del pacchetto.

--- trunk/se2-traveldream-ditucci-cavagnis-conteEJBClient/ejbModule/it/polimi/traveldream/ejb/management/ControlloCoerenzaMGR.java
+++ trunk/se2-traveldream-ditucci-cavagnis-conteEJBClient/ejbModule/it/polimi/traveldream/ejb/management/ControlloCoerenzaMGR.java
@@ -1,9 +1,12 @@
 package it.polimi.traveldream.ejb.management;
 
+import javax.ejb.Local;
+
 import it.polimi.traveldream.ejb.exception.CoerenzaException;
 import it.polimi.traveldream.ejb.management.dto.GiftListDTO;
 import it.polimi.traveldream.ejb.management.dto.PacchettoDTO;
 
+@Local
 public interface ControlloCoerenzaMGR {
 	
 	public void controlloPacchetto(PacchettoDTO p) throws CoerenzaException;
