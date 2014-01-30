056adea0fff66427e64c11968c2220c9751cfdf3
lorenzo.ditucci@gmail.com <lorenzo.ditucci@gmail.com@2a022a1e-1ce0-e600-22e9-f5664e8181da>
2014-01-30 22:28:37 +0000

    fix problema scaldafargis

--- se2-traveldream-ditucci-cavagnis-conteEJB/ejbModule/add_user_cliente.sql
+++ se2-traveldream-ditucci-cavagnis-conteEJB/ejbModule/add_user_cliente.sql
@@ -1,4 +1,4 @@
 --script per inserire un nuovo cliente. Username "cliente@cliente.it", password "cliente"
-INSERT INTO USERS VALUES ('cliente@cliente.it','user','cliente','cliente', 'cliente', '4983a0ab83ed86e0e7213c8783940193','2013-12-25 23:47:12', Null);
+INSERT INTO USERS VALUES ('cliente@cliente.it','user','Mario','Rossi', 'via Parrucchetti, Milano', '4983a0ab83ed86e0e7213c8783940193','2013-12-25 23:47:12', Null);
 INSERT INTO USERS_GROUPS VALUES ('cliente@cliente.it','USER');
 
